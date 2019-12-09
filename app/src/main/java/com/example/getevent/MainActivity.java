2cfb1082bee2de4b4417b7b4542759fd41b7267c
xan06 <xianhao2@illinois.edu>
2019-12-09 14:57:50 -0600

    Try TextView

--- app/src/main/java/com/example/getevent/MainActivity.java
+++ app/src/main/java/com/example/getevent/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.getevent;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -56,18 +59,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 //在子线程中执行Http请求，并将最终的请求结果回调到okhttp3.Callback中
+                //Http request is executed in a child thread, and make result to oKHttp3.Callback
                 HttpUtil.sendOkHttpRequest(API_BASE + input,new okhttp3.Callback(){
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
                         //得到服务器返回的具体内容
+                        //Get result
                         String responseData=response.body().string();
                         parseJSONWithGSON(responseData);
                     }
                     @Override
                     public void onFailure(Call call,IOException e){
                         //在这里进行异常情况处理
-                        TextView erroMsg = findViewById(R.id.errorMessage);
-                        erroMsg.setText(e.toString());
+                        //Error message
+                        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                        alertDialog.setTitle("Alert");
+                        alertDialog.setMessage(e.getMessage());
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
                     }
                 });
             }
@@ -76,19 +90,25 @@ public class MainActivity extends AppCompatActivity {
 
     private void parseJSONWithGSON(String jsonData) {
         //使用轻量级的Gson解析得到的json
+        //Create Gson
         Gson gson = new Gson();
         Result result = gson.fromJson(jsonData, Result.class);
         //显示UI界面，调用的showResponse方法
-        showResponse(result.getData().getEvents().get(0).toString());
+        //Setup UI
+        showResponse(result);
     }
-    private void showResponse(final String response) {
+    private void showResponse(final Result response) {
         //在子线程中更新UI
+        //Update UI in child thread
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 // 在这里进行UI操作，将结果显示到界面上
+                //Set up UI
                 TextView event1 = findViewById(R.id.event1);
-                event1.setText(response);
+                com.barnettwong.autofitcolortextview_library.AutoFitColorTextView event2 = findViewById(R.id.event2);
+                event1.setText(response.getData().getBirths().get(0).toString());
+                event2.setText(response.getData().getEvents().get(0).toString());
             }
         });
     }
